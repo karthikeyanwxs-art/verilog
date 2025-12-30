654c64d28be3896ca0552298c2d796b5814d259f
karthikeyanwxs-art <karthikeyanwxs@gmail.com>
2025-12-30 19:40:21 +0530

    andd gate

--- tb.v
+++ tb.v
@@ -1,25 +1,21 @@
-module tb;
-
+ module tb;
 reg A,B;
 wire Y;
-
-andgate gg(.a(A),,b(B),.y(Y));
-
+andgate gate(.a(A),.b(B),.y(Y));
 initial begin
-    A=1'b0,B=1'b0;
-    #10 A=1'b0,B=1'b1;
-    #10 A=1'b1,B=1'b0;
-    #10 A=1'b1,B=1'b1;
-    $finish;
-end
 
-always(*)begin
-    display("time=@ %t \t the input values \t A=%b,B=%b and the output is Y=%b",$time,A,B,Y);
-end
+    $monitor("time=@ %t \t the input values \t A=%b,B=%b and the output is Y=%b",$time,A,B,Y);
+  
+     A=1'b0;B=1'b0;
+    #10 A=1'b0;B=1'b1;
+    #10 A=1'b1;B=1'b0;
+    #10 A=1'b1;B=1'b1;
+  #10 $finish;
 
+end
+  
 initial begin 
-    $dumpfile(andgate.vcd);
-    $dumpars();
+  $dumpfile("andgate.vcd");
+    $dumpvars;
 end
-
 endmodule
